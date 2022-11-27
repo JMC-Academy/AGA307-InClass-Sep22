5bb0961e720ba3f8f76ca380420dd85744c30a86
BrendanVotano <78179739+JMC-Academy@users.noreply.github.com>
2022-11-28 10:56:11 +1100

    AI Basics

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.UI;
 
 public class Enemy : GameBehaviour
@@ -17,11 +18,16 @@ public class Enemy : GameBehaviour
 
     [Header("AI")]
     public PatrolType myPatrol;
-    public int patrolPoint = 0;            //Needed for linear patrol movement
-    public bool reverse = false;           //Needed for repeat patrol movement
-    public Transform startPos;             //Needed for repeat patrol movement
-    public Transform endPos;               //Needed for repeat patrol movement
-    public Transform moveToPos;
+    //public int patrolPoint = 0;            //Needed for linear patrol movement
+    //public bool reverse = false;           //Needed for repeat patrol movement
+    //public Transform startPos;             //Needed for repeat patrol movement
+    //public Transform endPos;               //Needed for repeat patrol movement
+    //public Transform moveToPos;
+    NavMeshAgent agent;
+    int currentWaypoint;
+    float detectDistance = 10f;
+    float detectTime = 5f;
+    float attackDistance = 2f;
 
     [Header("Health Bar")]
     public Slider healthBarSlider;
@@ -30,9 +36,9 @@ public class Enemy : GameBehaviour
     void Start()
     {
         anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
         Setup();
         SetupAI();
-        StartCoroutine(Move());
         transform.SetPositionAndRotation(transform.position, transform.rotation);
     }
 
@@ -65,17 +71,17 @@ public class Enemy : GameBehaviour
             case EnemyType.OneHand:
                 myHealth = 100f * healthModifier;
                 mySpeed = 2f * speedModifier;
-                myPatrol = PatrolType.Linear;
+                myPatrol = PatrolType.Patrol;
                 break;
             case EnemyType.TwoHand:
                 myHealth = 200f * healthModifier;
                 mySpeed = 1f * speedModifier;
-                myPatrol = PatrolType.Loop;
+                myPatrol = PatrolType.Patrol;
                 break;
             case EnemyType.Archer:
                 myHealth = 60f * healthModifier;
                 mySpeed = 5f * speedModifier;
-                myPatrol = PatrolType.Random;
+                myPatrol = PatrolType.Patrol;
                 break;
         }
         myMaxHealth = myHealth;
@@ -85,58 +91,86 @@ public class Enemy : GameBehaviour
 
     void SetupAI()
     {
-        startPos = Instantiate(new GameObject(), transform.position, transform.rotation).transform;
-        endPos = _EM.GetRandomSpawnPoint();
-        moveToPos = endPos;
+        currentWaypoint = UnityEngine.Random.Range(0, _EM.spawnPoints.Length);
+        agent.SetDestination(_EM.spawnPoints[currentWaypoint].position);
+        ChangeSpeed(mySpeed);
     }
 
-    IEnumerator Move()
+    void ChangeSpeed(float _speed)
     {
-        switch(myPatrol)
-        {
-            case PatrolType.Linear:
-                moveToPos = _EM.spawnPoints[patrolPoint];
-                patrolPoint = patrolPoint != _EM.spawnPoints.Length ? patrolPoint + 1 : 0;
-                break;
-            case PatrolType.Random:
-                moveToPos = _EM.GetRandomSpawnPoint();
-                break;
-            case PatrolType.Loop:
-                moveToPos = reverse ? startPos : endPos;
-                reverse = !reverse;
-                break;
-        }
-
-        transform.LookAt(moveToPos);
-
-        while (Vector3.Distance(transform.position, moveToPos.position) > 0.3f)
-        {
-            if(Vector3.Distance(_P.transform.position, transform.position) < attackRadius)
-            {
-                StopAllCoroutines();
-                StartCoroutine(Attack());
-                yield break;
-            }
-            transform.position = Vector3.MoveTowards(transform.position, moveToPos.position, Time.deltaTime * mySpeed);
-            yield return null;
-        }
-
-        yield return new WaitForSeconds(1);
-
-        StartCoroutine(Move());
+        agent.speed = _speed;
     }
 
     IEnumerator Attack()
     {
+        myPatrol = PatrolType.Attack;
+        ChangeSpeed(0);
         PlayAnimation("Attack");
-        yield return new WaitForSeconds(1);
-        StartCoroutine(Move());
+        yield return new WaitForSeconds(1.5f);
+        ChangeSpeed(mySpeed);
+        myPatrol = PatrolType.Chase;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.H))
-            Hit(10);
+        //Always get the distance between the player and me
+        float distToPlayer = Vector3.Distance(transform.position, _P.transform.position);
+
+        if (distToPlayer <= detectDistance && myPatrol != PatrolType.Attack)
+        {
+            if (myPatrol != PatrolType.Chase)
+            {
+                myPatrol = PatrolType.Detect;
+            }
+        }
+
+        //Set the animators Speed float value to that of my speed
+        anim.SetFloat("Speed", mySpeed);
+
+        switch (myPatrol)
+        {
+            case PatrolType.Patrol:
+                //Get the distance between the player and the curent waypoint
+                float distToWaypoint = Vector3.Distance(transform.position, _EM.spawnPoints[currentWaypoint].position);
+                //If the distance is close enough, get a new waypoint
+                if (distToWaypoint < 1)
+                    SetupAI();
+                //Reset the detect time
+                detectTime = 5;
+                break;
+
+            case PatrolType.Detect:
+                //Sets the destination to ourself, essentially stopping us
+                agent.SetDestination(transform.position);
+                //Stops our movement speed
+                ChangeSpeed(0);
+                //Decrement our detect time
+                detectTime -= Time.deltaTime;               
+                if (distToPlayer <= detectDistance)         
+                {
+                    myPatrol = PatrolType.Chase;
+                    detectTime = 5;
+                }
+                if (detectTime <= 0)
+                {
+                    myPatrol = PatrolType.Patrol;
+                    SetupAI();
+                }
+                break;
+
+            case PatrolType.Chase:
+                //Set the destination to that of the player
+                agent.SetDestination(_P.transform.position);
+                //Increse the speed of which to chase the chase the player
+                ChangeSpeed(mySpeed * 2);
+                //If the player gets outside our detect distance, go back to detect state
+                if (distToPlayer > detectDistance)
+                    myPatrol = PatrolType.Detect;
+                //If we are close to the player, then attack
+                if (distToPlayer <= attackDistance)
+                    StartCoroutine(Attack());
+                break;
+        }
     }
 
     void UpdateHealthBar()
@@ -163,8 +197,10 @@ public class Enemy : GameBehaviour
     void Die()
     {
         GetComponent<Collider>().enabled = false;
-        PlayAnimation("Die");
         StopAllCoroutines();
+        PlayAnimation("Die");
+        ChangeSpeed(0);
+        myPatrol = PatrolType.Die;
         GameEvents.ReportEnemyDie(this.gameObject);
     }
 
